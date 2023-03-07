1e6a8a44f85a5ad82702cd26ef53b463440f8818
nbonheur111 <56658293+nbonheur111@users.noreply.github.com>
2023-03-07 13:56:30 -0600

    summary section

--- src/pages/workout_history/recommendation/index.js
+++ src/pages/workout_history/recommendation/index.js
@@ -1,77 +1,133 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 const Recommendations = ({ workouts }) => {
-  console.log(workouts)
+  const [totalHoursAllTime, setTotalHoursAllTime] = useState(0);
+  const [totalHoursYTD, setTotalHoursYTD] = useState(0);
+  const [totalHoursThisMonth, setTotalHoursThisMonth] = useState(0);
+  const [totalHoursThisWeek, setTotalHoursThisWeek] = useState(0);
+  const [totalHoursLastWeek, setTotalHoursLastWeek] = useState(0);
+  const [totalHoursThisWeekToDate, setTotalHoursThisWeekToDate] = useState(0);
+  const [totalHoursLastWeekSameDay, setTotalHoursLastWeekSameDay] = useState(0);
 
-  //all time
-  const totalHoursAllTime = workouts.reduce((total, workout) =>{
-    return total + workout.duration;
-  }, 0 );
-
-  //total wokout YTD
-  const yearStart = new Date(new Date.getFullYear(), 0, 1) //start of current year
-  const workoutsYTD = workouts.filter((workout) =>new Date(workout.date) >= yearStart);
-  const totaHourslYTD = workoutsYTD.reduce((total, workout) => total + workout.duration, 0);
-
-  //This month
-  const monthStart = new Date( new Date().getFullYear(), new Date.getMonth(), 1)
-  const workoutsThisMonth = workouts.filter((workout) => new Date(workout.date) >= monthStart);
-  const totalHoursThisMOnth = workoutsThisMonth.reduce((total, workout) => total + workout.duration, 0)
-
-  //this week
-  const weekStart = new Date(new Date() - (new Date().getDay() * 86400000) ); //start of current week
-  //86,400,000 is miliseconds in  24hrs, while new Date().getDay() will return a day as an index
-
-    //   const today = new Date();
-    // const dayOfWeek = today.getDay();
-    // const daysSinceMonday = (dayOfWeek + 6) % 7; 
-    // const monday = new Date(today);
-    // monday.setDate(today.getDate() - daysSinceMonday);
-    // const weekStart = new Date(monday.getFullYear(), monday.getMonth(), monday.getDate());
-
-    const workoutsThisWeek = workouts.filter((workout) => new Date(workout.date) >= weekStart);
-   const totalHoursThisWeek =  workoutsThisWeek.reduce((total, workout) =>  total + workout.duration, 0)
-
-    //last week
-    const lastWeekStart = new Date(weekStart - 7*86400000) //previous week start
-    const lastWeekEnd = new Date(weekStart - 86400000)//end of previous week
-    const workoutsLastWeek = workouts.filter((workout) => new Date(workout.date) >= lastWeekStart && new Date(workout.date) <= lastWeekEnd)
-    const totalHoursLastWeek= workoutsLastWeek.reduce((total, workout) => total + workout.duration, 0);
-
-    //week before last
-    const weekBeforeStart = new Date(lastWeekStart - 7*86400000) //start of week before last
-    const weekBeforeEnd = new Date(lastWeekStart - 86400000)//end of week before last
-    const workoutsWeekBefore = workouts.filter((workout) => new Date(workout.date) >= weekBeforeStart && new Date(workout.date) <= weekBeforeEnd)
-    const totalHoursWeekBefore = workoutsWeekBefore.reduce((total, workout) => total + workout.duration, 0);
-
-    // Compare this week to last week
-    const weekStartDate = new Date(new Date() - new Date().getDay() * 86400000);
-    const lastWeekStartDate = new Date(weekStartDate - 7 * 86400000); // start of previous week
-    const lastWeekEndDate = new Date(weekStartDate - 1 * 86400000); // end of previous week
-    const weekToDate = totalHoursThisWeek;
-    const lastWeekSameDays = workoutsLastWeek.filter(
-      (workout) => new Date(workout.date) >= weekStartDate
-    );
-    const lastWeekSameDaysTotal = lastWeekSameDays.reduce(
+  useEffect(() => {
+    //all time
+    const totalHours = workouts.reduce(
       (total, workout) => total + workout.duration,
       0
     );
-    const weekComparison = (weekToDate / lastWeekSameDaysTotal ) * 100
+    setTotalHoursAllTime(totalHours);
 
+    //total workout YTD
+    const yearStart = new Date(new Date().getFullYear(), 0, 1); //start of current year
+    const workoutsYTD = workouts.filter(
+      (workout) => new Date(workout.date) >= yearStart
+    );
+    const totalHoursYTD = workoutsYTD.reduce(
+      (total, workout) => total + workout.duration,
+      0
+    );
+    setTotalHoursYTD(totalHoursYTD);
 
+    //This month
+    const monthStart = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
+    const workoutsThisMonth = workouts.filter(
+      (workout) => new Date(workout.date) >= monthStart
+    );
+    const totalHoursThisMonth = workoutsThisMonth.reduce(
+      (total, workout) => total + workout.duration,
+      0
+    );
+    setTotalHoursThisMonth(totalHoursThisMonth);
 
+    //this week
+    const weekStart = new Date(
+      new Date() - new Date().getDay() * 86400000
+    ); //start of current week
+    const workoutsThisWeek = workouts.filter(
+      (workout) => new Date(workout.date) >= weekStart
+    );
+    const totalHoursThisWeek = workoutsThisWeek.reduce(
+      (total, workout) => total + workout.duration,
+      0
+    );
+    setTotalHoursThisWeek(totalHoursThisWeek);
 
+    //last week
+    const lastWeekStart = new Date(weekStart - 7 * 86400000); //previous week start
+    const lastWeekEnd = new Date(weekStart - 86400000); //end of previous week
+    const workoutsLastWeek = workouts.filter(
+      (workout) =>
+        new Date(workout.date) >= lastWeekStart &&
+        new Date(workout.date) <= lastWeekEnd
+    );
+    const totalHoursLastWeek = workoutsLastWeek.reduce(
+      (total, workout) => total + workout.duration,
+      0
+    );
+    setTotalHoursLastWeek(totalHoursLastWeek);
 
-  return (
-    <div className="recommendations">
-      <h2>Recommendations</h2>
-      <ul>
-        <li>Recommendation 1 coming soon</li>
-        <li>Recommendation 2 coming soon</li>
-        <li>Recommendation 3 coming soon</li>
-      </ul>
+    //this week to date
+      const today = new Date();
+      const workoutsThisWeekToDate = workouts.filter(
+      (workout) => new Date(workout.date) >= weekStart && new Date(workout.date) <= today
+      );
+      const totalHoursThisWeekToDate = workoutsThisWeekToDate.reduce(
+      (total, workout) => total + workout.duration,
+      0
+      );
+
+    //last week to date
+    const lastWeekStartDate = new Date(weekStart - 7 * 86400000);
+    const lastWeekEndDate = new Date(weekStart - 86400000);
+    const workoutsLastWeekToDate = workouts.filter(
+    (workout) =>
+    new Date(workout.date) >= lastWeekStartDate && new Date(workout.date) <= lastWeekEndDate
+    );
+    const totalHoursLastWeekToDate = workoutsLastWeekToDate.reduce(
+    (total, workout) => total + workout.duration,
+    0
+    );
+
+    //compare this week to date to last week same period
+    const diffThisWeekToDateLastWeek =
+    totalHoursThisWeekToDate - totalHoursLastWeekToDate;
+
+    setTotalHoursThisWeekToDate(totalHoursThisWeekToDate);
+    setTotalHoursLastWeekSameDay(totalHoursLastWeekToDate);
+
+    }, [workouts]);
+
+return (
+    <div>
+        <h2>Summary:</h2>
+          <p>
+          {/* Total hours spent working out all time: {totalHoursAllTime.toFixed(1)/60}{" "}
+          hours */}
+          </p>
+          <p>
+          YTD: {totalHoursYTD.toFixed(1)/60} hours
+          </p>
+           <p>
+          This month:{" "}
+          {totalHoursThisMonth.toFixed(1)/60} hours
+          </p>
+          <p>Last week Total: {totalHoursLastWeek.toFixed(1)/60} hours</p>
+          <p>This week: {totalHoursThisWeek.toFixed(1)/60} hours</p>
+
+          <p>
+          This week so far:{" "}
+          {totalHoursThisWeekToDate.toFixed(1)/60} hours
+          </p>
+          <p>
+          Last week you were at:{" "}
+          {totalHoursLastWeekSameDay.toFixed(1)/60} hours
+          </p>
+          <p>
+          {/* Difference between this week up to today and same period last week:{" "} */}
+          {/* {diffThisWeekToDateLastWeek.toFixed(2)} hours */}
+          </p>
     </div>
-  );
+);
 };
 
 export default Recommendations;
